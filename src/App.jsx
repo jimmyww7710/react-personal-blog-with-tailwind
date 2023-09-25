104ef54b7fe1bafdd8dc97ab66730cf9cee63774
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2023-09-25 17:52:57 +0800

    display articles make them line five in a row.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,32 @@ import { useState, useEffect } from 'react';
 import PersonalInfo from "./components/PersonalInfo";
 import Tabs from "./components/Tabs";
 import FilterInput from './components/FilterInput';
+const articles = [
+  {
+    "title": "t01",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    "title": "t02",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    "title": "t03",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    "title": "t04",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    "title": "t05",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    "title": "t06",
+    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }
+];
 const App = () => {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
   useEffect(() => {
@@ -15,7 +41,25 @@ const App = () => {
       <PersonalInfo />
       <Tabs currentTabIndex={currentTabIndex} onChangeTabIndex={handleCurrentTabIndex} />
       <FilterInput />
-
+      <div className='wrapper'>
+        {
+          currentTabIndex == 0 && (
+            <div className="grid grid-cols-4 gap-4">
+              {articles.map((element, index) => (
+                <div className="card card-compact bg-base-100 shadow-xl" key={index}>
+                  <div className="card-body">
+                    <h2 className="card-title">{element.title}</h2>
+                    <p>{element.summary}</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">read more</button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )
+        }
+      </div>
     </div>
   )
 }
